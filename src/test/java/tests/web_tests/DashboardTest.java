e3f574bccbbfbc4e3a33983c61d912dba94bbfa8
Razan.Yousef <Razan.yousef@maf.ae>
2022-11-22 09:08:20 +0200

    adding dashboard

---
+++ src/test/java/tests/web_tests/DashboardTest.java
@@ -0,0 +1,19 @@
+package tests.web_tests;
+import com.github.javafaker.Faker;
+import org.maf.core.instance.BetaDriver.base;
+import org.maf.page_objects.*;
+import org.maf.utils.Common_steps.SharedSteps;
+import org.maf.utils.ExtentReport.TestListener;
+import org.maf.utils.common.SharedMethods;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class)
+
+public class DashboardTest extends base {
+
+}
+
+
